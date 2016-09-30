87de2ae9060df195fe24a0326b8e2cc563a2ac6e
Jordan Clark <jordan.clark@esu10.org>
2016-09-30 14:07:03 -0500

    fixed an issue with centos7 and moving bin files

--- container-files/etc/cont-init.d/00_bootstrap.sh
+++ container-files/etc/cont-init.d/00_bootstrap.sh
@@ -1,6 +1,37 @@
-#!/usr/bin/with-contenv /bin/sh
+#!/usr/bin/with-contenv sh
 
 set -e
+
+#Fix some moved bin files
+if [ ! -f "/bin/sh" ]; then
+  if [ ! -L "/bin/sh" ]; then
+		if [ -f "/usr/bin/sh" ]; then
+			ln -s /usr/bin/sh /bin/sh
+		fi
+  fi
+fi
+if [ ! -f "/bin/bash" ]; then
+  if [ ! -L "/bin/bash" ]; then
+		if [ -f "/usr/bin/bash" ]; then
+			ln -s /usr/bin/bash /bin/bash
+		fi
+  fi
+fi
+if [ ! -f "/bin/true" ]; then
+  if [ ! -L "/bin/true" ]; then
+		if [ -f "/usr/bin/true" ]; then
+			ln -s /usr/bin/true /bin/true
+		fi
+  fi
+fi
+if [ ! -f "/bin/false" ]; then
+  if [ ! -L "/bin/false" ]; then
+		if [ -f "/usr/bin/false" ]; then
+			ln -s /usr/bin/false /bin/false
+		fi
+  fi
+fi
+
 
 #User params
 
