6455591e8f4ebce1aee40c1ac1ec033a191a85a3
Jordan Clark <jordan.clark@esu10.org>
2017-09-20 15:33:48 -0500

    reworked the fix-bin.sh and fixed the exit code not passing through

--- container-files/usr/local/sbin/fix-bins.sh
+++ container-files/usr/local/sbin/fix-bins.sh
@@ -1,31 +1,10 @@
-#!sh
+#!/usr/bin/env sh
 
-#Fix some moved bin files
-if [ ! -f "/bin/sh" ]; then
-  if [ ! -L "/bin/sh" ]; then
-		if [ -f "/usr/bin/sh" ]; then
-			ln -s /usr/bin/sh /bin/sh
-		fi
-  fi
-fi
-if [ ! -f "/bin/bash" ]; then
-  if [ ! -L "/bin/bash" ]; then
-		if [ -f "/usr/bin/bash" ]; then
-			ln -s /usr/bin/bash /bin/bash
-		fi
-  fi
-fi
-if [ ! -f "/bin/true" ]; then
-  if [ ! -L "/bin/true" ]; then
-		if [ -f "/usr/bin/true" ]; then
-			ln -s /usr/bin/true /bin/true
-		fi
-  fi
-fi
-if [ ! -f "/bin/false" ]; then
-  if [ ! -L "/bin/false" ]; then
-		if [ -f "/usr/bin/false" ]; then
-			ln -s /usr/bin/false /bin/false
-		fi
-  fi
+#Fix the link that S6 Overlay blew away
+if [ ! -L "/bin" ]; then
+  cd / || exit 1
+  tar cf - bin | ( cd /usr || exit 1; tar xfp - )
+  rm -f -- bin/*
+  rmdir bin
+  ln -s /usr/bin bin
 fi
